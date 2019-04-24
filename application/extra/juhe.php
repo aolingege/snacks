cd259c333ebb8dd1c733800784adf8b04e260c13
奥林 <32017297+aolingege@users.noreply.github.com>
2019-04-24 14:59:32 +0800

    Update juhe.php

--- application/extra/juhe.php
+++ application/extra/juhe.php
@@ -7,7 +7,7 @@
  */
 return [
     //查快递秘钥
-    'expressKey' => 'd736ce218fe934a08215e23170bbd51c',
+    'expressKey' => '',
     //查快递基地址
     'expressUrl' => 'http://v.juhe.cn/exp/index',
     //查快递公司基地址
@@ -15,5 +15,5 @@ return [
     //查彩票基地址
     'lotteryUrl' => 'http://apis.juhe.cn/lottery',
     //查彩票秘钥
-    'lotteryKey' => '99ea773432ccf3d9a01a9abfc230ca99',
+    'lotteryKey' => '',
 ];
